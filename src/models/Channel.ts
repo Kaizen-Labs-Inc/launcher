e6e36037cc05dcfdcf86588e612e72c6fd6f0931
Jordan Fulghum <j.fulghum@gmail.com>
2021-09-23 07:05:54 -0400

    add tippy

---
+++ src/models/Channel.ts
@@ -0,0 +1,82 @@
+export default interface Channel {
+	id: string;
+	title: string;
+	url: string;
+	description?: string;
+	icon?: string; // TODO support image paths and/or emojis
+}
+
+export const mockChannels = [
+	{
+		id: 1,
+		icon: '✉️',
+		title: 'Gmail',
+		url: 'mail.google.com',
+		description: 'Lorem ipsum'
+	},
+	{
+		id: 2,
+		icon: '🗓',
+		title: 'Calendar',
+		url: 'calendar.google.com',
+
+		description: 'Lorem ipsum'
+	},
+
+	{
+		id: 3,
+		icon: '💬',
+		title: 'Slack',
+		url: 'slack.com',
+		description: 'Lorem ipsum'
+	},
+	{
+		id: 4,
+		icon: '📓',
+		title: 'Notion',
+		url: 'notion.so',
+		description: 'Lorem ipsum'
+	},
+	{
+		id: 5,
+		icon: '📬',
+		title: 'Customer.io',
+		url: 'customer.io',
+		description: 'Lorem ipsum'
+	},
+	{
+		id: 6,
+		icon: '🖇',
+		title: 'Trello',
+		url: 'trello.com',
+		description: 'Lorem ipsum'
+	},
+	{
+		id: 7,
+		icon: '📈',
+		title: 'Mixpanel',
+		url: 'mixpanel.com',
+		description: 'Lorem ipsum'
+	},
+	{
+		id: 7,
+		icon: '💸',
+		title: 'Rippling',
+		url: 'rippling.com',
+		description: 'Lorem ipsum'
+	},
+	{
+		id: 7,
+		icon: '☎',
+		title: 'Zendesk',
+		url: 'zendesk.com',
+		description: 'Lorem ipsum'
+	},
+	{
+		id: 7,
+		icon: '💼',
+		title: 'Salesforce',
+		url: 'salesforce.com',
+		description: 'Lorem ipsum'
+	}
+];
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,11 +6,11 @@
 	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
 
-	import { goto } from '$app/navigation';
+	import { mockChannels } from '../models/Channel';
 	import { SearchIcon, PlusCircleIcon } from 'svelte-feather-icons';
 	let query = '';
 	let searchIsFocused: boolean = false;
-	$: filteredChannels = channels.filter(
+	$: filteredChannels = mockChannels.filter(
 		(channel) => channel.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
 	);
 	onMount(() => {
@@ -18,7 +18,12 @@
 			'keydown',
 			(event) => {
 				let searchInput = document.getElementById('searchInput');
-				if (event.metaKey && event.key === 'k' && searchInput !== document.activeElement) {
+				if (
+					event.metaKey &&
+					event.key === 'k' &&
+					searchInput !== document.activeElement &&
+					!searchIsFocused
+				) {
 					searchIsFocused = true;
 					searchInput.focus();
 				}
@@ -26,80 +31,6 @@
 			false
 		);
 	});
-	const channels = [
-		{
-			id: 1,
-			icon: '✉️',
-			title: 'Gmail',
-			url: 'mail.google.com',
-			description: 'Lorem ipsum'
-		},
-		{
-			id: 2,
-			icon: '🗓',
-			title: 'Calendar',
-			url: 'calendar.google.com',
-
-			description: 'Lorem ipsum'
-		},
-
-		{
-			id: 3,
-			icon: '💬',
-			title: 'Slack',
-			url: 'slack.com',
-			description: 'Lorem ipsum'
-		},
-		{
-			id: 4,
-			icon: '📓',
-			title: 'Notion',
-			url: 'notion.so',
-			description: 'Lorem ipsum'
-		},
-		{
-			id: 5,
-			icon: '📬',
-			title: 'Customer.io',
-			url: 'customer.io',
-			description: 'Lorem ipsum'
-		},
-		{
-			id: 6,
-			icon: '🖇',
-			title: 'Trello',
-			url: 'trello.com',
-			description: 'Lorem ipsum'
-		},
-		{
-			id: 7,
-			icon: '📈',
-			title: 'Mixpanel',
-			url: 'mixpanel.com',
-			description: 'Lorem ipsum'
-		},
-		{
-			id: 7,
-			icon: '💸',
-			title: 'Rippling',
-			url: 'rippling.com',
-			description: 'Lorem ipsum'
-		},
-		{
-			id: 7,
-			icon: '☎',
-			title: 'Zendesk',
-			url: 'zendesk.com',
-			description: 'Lorem ipsum'
-		},
-		{
-			id: 7,
-			icon: '💼',
-			title: 'Salesforce',
-			url: 'salesforce.com',
-			description: 'Lorem ipsum'
-		}
-	];
 </script>
 
 <svelte:head>
@@ -110,7 +41,6 @@
 		<ul class="flex flex-row justify-end text-gray-500">
 			<li class="cursor-pointer mr-6 hover:text-white">Kaizen Labs</li>
 			<li class="cursor-pointer mr-6 hover:text-white">Jordan</li>
-			<li class="cursor-pointer hover:text-white">Sign out</li>
 		</ul>
 	</nav>
 	<section class="w-full mt-16 flex justify-between">
