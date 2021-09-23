21ef1500c0668bf5c19cea3a83a2f363bfa6df1c
Jordan Fulghum <j.fulghum@gmail.com>
2021-09-23 16:55:37 -0400

    Add tags

--- src/components/AddFormPopover.svelte
+++ src/components/AddFormPopover.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-
 	import { scale } from 'svelte/transition';
 	import { PlusCircleIcon } from 'svelte-feather-icons';
 	import Popover from 'svelte-popover';
+	import Tags from 'svelte-tags-input';
 	let popOverIsFocused: boolean = false;
 
 	onMount(() => {
@@ -52,7 +52,7 @@
 				<input
 					name="url"
 					autofocus
-					type="text"
+					type="url"
 					id="url"
 					placeholder="Paste or type a URL"
 					class="bg-gray-200 rounded p-2"
@@ -73,12 +73,15 @@
 			</div>
 			<div class="my-4 flex flex-col">
 				<label for="tags" class="font-medium text-gray-500">Tag it</label>
-				<input
+				<div class="tagsContainer">
+					<Tags onlyUnique />
+				</div>
+				<!-- <input
 					name="tags"
 					type="text"
 					placeholder="Add tags to help you and others find it"
 					class="bg-gray-200 rounded p-2"
-				/>
+				/> -->
 			</div>
 			<div
 				class="flex cursor-pointer justify-center items-center rounded bg-black text-white font-medium py-2 text-lg"
@@ -88,3 +91,34 @@
 		</form>
 	</div>
 </Popover>
+
+<style>
+	.tagsContainer :global(.svelte-tags-input) {
+		background: transparent;
+		padding-top: 4px;
+		padding-bottom: 4px;
+		font-size: 16px;
+	}
+	.tagsContainer :global(.svelte-tags-input-tag) {
+		border-radius: 4px;
+		background: black;
+		font-size: 16px;
+		padding: 4px;
+	}
+
+	.tagsContainer :global(.svelte-tags-input-tag-remove) {
+		margin-left: 5px;
+	}
+	.tagsContainer :global(.svelte-tags-input-layout) {
+		background-color: #e5e7eb;
+		border: none;
+		outline: none;
+	}
+
+	.tagsContainer :global(.svelte-tags-input-layout:focus) {
+		border: none;
+	}
+	.tagsContainer :global(.svelte-tags-input-layout:hover) {
+		border: none;
+	}
+</style>
