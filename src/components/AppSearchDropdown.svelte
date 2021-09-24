cc196b308ac354bf9fcfde2e602eeff46045e18e
Jordan Fulghum <j.fulghum@gmail.com>
2021-09-24 16:23:39 -0400

    add empty state

--- src/components/AppSearchDropdown.svelte
+++ src/components/AppSearchDropdown.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import type Channel from 'src/models/Channel';
-
 	import { Edit2Icon } from 'svelte-feather-icons';
 
 	export let selectedChannelIndex;
@@ -10,67 +8,76 @@
 </script>
 
 <section
-	style="max-height: 500px"
-	class="flex flex-col w-full bg-white bg-opacity-10 rounded-b-lg overflow-y-scroll overflow-x-visible"
+	style="max-height: 500px;"
+	class="flex flex-col w-full bg-white bg-opacity-10 rounded-b-lg overflow-y-scroll shadow-2xl"
 >
 	<!-- TODO Have 2 states. A) by last used, when b) alpha  -->
-	{#each filteredChannels.sort((a, b) => a.title.localeCompare(b.title)) as channel, i}
-		<div
-			on:click|preventDefault={() => {
-				// TODO this click is getting intercepted somehow
-				onSelect(channel);
-			}}
-			on:mouseover={() => {
-				selectedChannelIndex = i;
-			}}
-			on:focus
-			class="grid grid-cols-4 gap-12 text-left rounded-2xl cursor-pointer p-6 transition duration-200 ease-in-out  {selectedChannelIndex ===
-			i
-				? 'bg-white bg-opacity-10 backdrop-blur-md scale-105'
-				: 'bg-transparent'}
-            "
-		>
-			<div class="flex flex-row">
-				<div class="icon flex-shrink-0 w-14 h-14 flex items-center justify-center mr-4">
-					<img class="w-8 h-8" src={channel.iconImageUrl} alt={channel.title} />
-				</div>
-				<div class="text-2xl">
-					{channel.title}
-					<div class="block text-sm {selectedChannelIndex === i ? 'opacity-50' : 'opacity-25'}">
-						{channel.url}
-					</div>
-				</div>
-			</div>
-			{#if channel.description}
-				<div class={selectedChannelIndex === i ? 'opacity-50' : 'opacity-25'}>
-					{channel.description}
-				</div>
-			{:else}
-				<div />
-			{/if}
-			{#if channel.tags}
-				<div class="flex flex-wrap flex-row">
-					{#each channel.tags as tag}
-						<div
-							class="flex justify-center items-center h-8 mr-2 mb-2 text-sm font-medium rounded bg-white bg-opacity-5 px-2 py-1"
-						>
-							#{tag}
-						</div>
-					{/each}
-				</div>
-			{:else}
-				<div />
-			{/if}
-			<div
-				on:click|preventDefault={() => {
-					console.log('edit clicked');
-				}}
-				class="w-12 h-12 hover:bg-white hover:bg-opacity-10 rounded-md flex justify-center items-center"
-			>
-				<Edit2Icon size="20" strokeWidth="1" />
+	{#if filteredChannels.length === 0}
+		<div class="mt-16 mb-16 flex flex-col justify-center items-center">
+			<p class="text-2xl ">🤷‍♂️ Nothing found.</p>
+			<div class="bg-white bg-opacity-20 cursor-pointer text-lg font-medium mt-8 p-3 rounded">
+				Add something
 			</div>
 		</div>
-	{/each}
+	{:else}
+		{#each filteredChannels.sort((a, b) => a.title.localeCompare(b.title)) as channel, i}
+			<div
+				on:click|preventDefault={() => {
+					// TODO this click is getting intercepted somehow
+					onSelect(channel);
+				}}
+				on:mouseover={() => {
+					selectedChannelIndex = i;
+				}}
+				on:focus
+				class="grid grid-cols-4 gap-12 text-left cursor-pointer p-6 transition duration-200 ease-in-out  {selectedChannelIndex ===
+				i
+					? 'bg-white bg-opacity-10 backdrop-blur-md'
+					: 'bg-transparent'}
+            "
+			>
+				<div class="flex flex-row">
+					<div class="icon flex-shrink-0 w-14 h-14 flex items-center justify-center mr-4">
+						<img class="w-8 h-8" src={channel.iconImageUrl} alt={channel.title} />
+					</div>
+					<div class="text-2xl">
+						{channel.title}
+						<div class="block text-sm {selectedChannelIndex === i ? 'opacity-50' : 'opacity-25'}">
+							{channel.url}
+						</div>
+					</div>
+				</div>
+				{#if channel.description}
+					<div class={selectedChannelIndex === i ? 'opacity-50' : 'opacity-25'}>
+						{channel.description}
+					</div>
+				{:else}
+					<div />
+				{/if}
+				{#if channel.tags}
+					<div class="flex flex-wrap flex-row">
+						{#each channel.tags as tag}
+							<div
+								class="flex justify-center items-center h-8 mr-2 mb-2 text-sm font-medium rounded bg-white bg-opacity-5 px-2 py-1"
+							>
+								#{tag}
+							</div>
+						{/each}
+					</div>
+				{:else}
+					<div />
+				{/if}
+				<div
+					on:click|preventDefault={() => {
+						console.log('edit clicked');
+					}}
+					class="w-12 h-12 hover:bg-white hover:bg-opacity-10 rounded-md flex justify-center items-center"
+				>
+					<Edit2Icon size="20" strokeWidth="1" />
+				</div>
+			</div>
+		{/each}
+	{/if}
 </section>
 
 <style>
