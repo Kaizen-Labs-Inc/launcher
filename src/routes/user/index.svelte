b369ee23d07196722f38966560cd99f24e66f843
jordanful <j.fulghum@gmail.com>
2021-11-18 12:56:00 -0500

    restyle sign in page

--- src/routes/user/index.svelte
+++ src/routes/user/index.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+
 	import { session } from '$app/stores';
 	import { signOut } from 'sk-auth/client';
 
@@ -8,10 +10,22 @@
 		console.log(value);
 		user = value?.user?.connections?.google;
 	});
+
+	const logout = () => {
+		signOut();
+		session.set(undefined);
+		goto('/');
+	};
 </script>
 
 <div class="flex flex-col justify-center items-center mt-16">
 	<img src={user.picture} class="rounded-full" />
 	<div class="mt-4 text-xl">{user.name}</div>
 	<div class="opacity-50 mt-1">{user.email}</div>
+	<div
+		on:click={logout}
+		class="rounded px-4 py-1 text-lg bg-white flex items-center justify-center text-black cursor-pointer mt-4"
+	>
+		Sign out
+	</div>
 </div>
