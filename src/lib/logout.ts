c1579f28eea0056a39d78ccf2ee0082354ad2c75
Nate Vaughan <nate.vaughan@protonmail.com>
2021-11-19 09:40:29 -0600

    COCO fix logout

--- src/lib/logout.ts
+++ src/lib/logout.ts
@@ -1,9 +1,9 @@
 import { signOut } from 'sk-auth/client';
-import { session } from '$app/stores';
 import { goto } from '$app/navigation';
+import { userStore } from '../stores/userStore';
 
 export const logout = () => {
 	signOut();
-	session.set(undefined);
+	userStore.set(undefined);
 	goto('/');
 };
