82d47cbb0350014ec2091ee6fb43f5a19af8dd42
jordanful <j.fulghum@gmail.com>
2021-11-20 19:05:51 -0500

    add CTA on landing page

--- src/components/LandingPage.svelte
+++ src/components/LandingPage.svelte
@@ -1,15 +1,36 @@
 <script>
 	import { mockChannels } from '../model/Channel';
-	import { scale } from 'svelte/transition';
+	let ctaIsHovered = false;
+
+	const handleCtaHover = () => {
+		ctaIsHovered = true;
+	};
 </script>
 
 <div class="container">
 	<nav class="flex w-full justify-end">
 		<a href="/sign-in" class="opacity-50 hover:opacity-100 no-underline">Sign in</a>
 	</nav>
-	<div class="text-center">
-		<div class="uppercase text-xl opacity-50 mt-24 mb-4">Springboard</div>
-		<h1 class="text-6xl font-medium">A home screen for your growing team.</h1>
+	<div class="text-center mt-24 flex flex-col">
+		<h1 class="text-6xl font-medium leading-relaxed">A home screen for your growing team.</h1>
+		<h2 class="text-3xl mt-6">
+			<span class="opacity-50">Create shortcuts to your team's daily apps with</span>
+			<span class="opacity-100">Springboard</span><span class="opacity-50">.</span>
+		</h2>
+		<div
+			on:focus
+			on:blur
+			on:mouseover={() => {
+				ctaIsHovered = true;
+			}}
+			on:mouseout={() => {
+				ctaIsHovered = false;
+			}}
+			class="cta mx-auto rounded-md cursor-pointer text-2xl font-medium py-3 px-6 flex items-center justify-center mt-10 text-black"
+		>
+			Play with a demo
+		</div>
+		<div class="mt-3 {ctaIsHovered ? 'opacity-100' : 'opacity-60'}">No sign-up required 🙂</div>
 	</div>
 	<section class="mt-24 grid md:grid-cols-4 grid-cols-2 gap-8 transition duration-200 ease-in-out ">
 		{#each mockChannels.slice(0, 8) as channel, i (channel.id)}
@@ -43,10 +64,16 @@
 			#6dd5fa,
 			#2980b9
 		); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
 		-webkit-background-clip: text;
 		-webkit-text-fill-color: transparent;
 	}
+
+	.cta {
+		background-color: #85ffbd;
+		background-image: linear-gradient(45deg, #85ffbd 0%, #fffb7d 100%);
+		width: fit-content;
+	}
+
 	.icon {
 		width: 120px;
 		height: 120px;
