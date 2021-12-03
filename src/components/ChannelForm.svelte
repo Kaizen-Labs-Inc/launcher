99b65fd51fe251532a817d8877b24a9c1d72dcf9
Nate Vaughan <nate.vaughan@protonmail.com>
2021-12-03 10:02:29 -0600

    Fix botched merge

--- src/components/ChannelForm.svelte
+++ src/components/ChannelForm.svelte
@@ -7,21 +7,20 @@
 	import { EmojiButton } from '@joeattardi/emoji-button';
 	import { SmileIcon } from 'svelte-feather-icons';
 	import Tags from 'svelte-tags-input';
-	import type MockChannel from 'src/model/MockChannel';
+	import type Channel from 'src/model/Channel';
 	// import { scrape } from '../routes/api/scrape';
 	const dispatch = createEventDispatcher();
-	export let channel: MockChannel = {
-		name: '',
+	export let channel: Channel = {
+		title: '',
 		description: '',
 		url: '',
 		icon: '',
-		emoji: '',
 		tags: []
 	};
 	let emoji;
 	let picker;
 
-	export const handleSubmit = (channel: MockChannel) => {
+	export const handleSubmit = (channel: Channel) => {
 		channel.emoji = emoji;
 		dispatch('submit', {
 			channel: channel
@@ -120,32 +119,32 @@
 </div>
 
 <style>
-	.tagsContainer :global(.svelte-tags-input) {
-		background: transparent;
-		padding-top: 4px;
-		padding-bottom: 4px;
-		font-size: 16px;
-	}
-	.tagsContainer :global(.svelte-tags-input-tag) {
-		border-radius: 3px;
-		background: rgba(255, 255, 255, 0.1);
-		font-size: 16px;
-		padding: 4px;
-	}
+    .tagsContainer :global(.svelte-tags-input) {
+        background: transparent;
+        padding-top: 4px;
+        padding-bottom: 4px;
+        font-size: 16px;
+    }
+    .tagsContainer :global(.svelte-tags-input-tag) {
+        border-radius: 3px;
+        background: rgba(255, 255, 255, 0.1);
+        font-size: 16px;
+        padding: 4px;
+    }
 
-	.tagsContainer :global(.svelte-tags-input-tag-remove) {
-		margin-left: 5px;
-	}
-	.tagsContainer :global(.svelte-tags-input-layout) {
-		background-color: rgba(255, 255, 255, 0.1);
-		border: none;
-		outline: none;
-	}
+    .tagsContainer :global(.svelte-tags-input-tag-remove) {
+        margin-left: 5px;
+    }
+    .tagsContainer :global(.svelte-tags-input-layout) {
+        background-color: rgba(255, 255, 255, 0.1);
+        border: none;
+        outline: none;
+    }
 
-	.tagsContainer :global(.svelte-tags-input-layout:focus) {
-		border: none;
-	}
-	.tagsContainer :global(.svelte-tags-input-layout:hover) {
-		border: none;
-	}
+    .tagsContainer :global(.svelte-tags-input-layout:focus) {
+        border: none;
+    }
+    .tagsContainer :global(.svelte-tags-input-layout:hover) {
+        border: none;
+    }
 </style>
