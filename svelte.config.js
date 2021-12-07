fff3620f5bfb360b9cbe9193d214add9c7314d7c
jordanful <j.fulghum@gmail.com>
2021-12-07 15:44:35 -0500

    remove unused config field

--- svelte.config.js
+++ svelte.config.js
@@ -14,12 +14,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: vercel(),
-		vite: {
-			define: {
-				'process.env': process.env
-			}
-		}
+		adapter: vercel()
 	}
 };
 
