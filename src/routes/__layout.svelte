3f472eb7eb8661429cbfabde320374dd6539ed60
jordanful <j.fulghum@gmail.com>
2021-12-07 16:25:37 -0500

    extract vars into module

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,7 +7,7 @@
 	import type GoogleUser from '../model/api/GoogleUser';
 	import LoadingIndicator from '../components/LoadingIndicator.svelte';
 	import { goto } from '$app/navigation';
-
+	import { variables } from '$lib/env';
 	import AuthenticatedNav from '../components/nav/AuthenticatedNav.svelte';
 	import PublicNav from '../components/nav/PublicNav.svelte';
 
@@ -20,60 +20,60 @@
 	let loading: boolean = true;
 
 	onMount(async () => {
-		// const analytics = (window.analytics = window.analytics || []);
+		const analytics = (window.analytics = window.analytics || []);
 
-		// if (!analytics.initialize)
-		// 	if (analytics.invoked)
-		// 		window.console && console.error && console.error('Segment snippet included twice.');
-		// 	else {
-		// 		analytics.invoked = !0;
-		// 		analytics.methods = [
-		// 			'trackSubmit',
-		// 			'trackClick',
-		// 			'trackLink',
-		// 			'trackForm',
-		// 			'pageview',
-		// 			'identify',
-		// 			'reset',
-		// 			'group',
-		// 			'track',
-		// 			'ready',
-		// 			'alias',
-		// 			'debug',
-		// 			'page',
-		// 			'once',
-		// 			'off',
-		// 			'on',
-		// 			'addSourceMiddleware',
-		// 			'addIntegrationMiddleware',
-		// 			'setAnonymousId',
-		// 			'addDestinationMiddleware'
-		// 		];
-		// 		analytics.factory = function (e) {
-		// 			return function () {
-		// 				const t = Array.prototype.slice.call(arguments);
-		// 				t.unshift(e);
-		// 				analytics.push(t);
-		// 				return analytics;
-		// 			};
-		// 		};
-		// 		for (let e = 0; e < analytics.methods.length; e++) {
-		// 			const key = analytics.methods[e];
-		// 			analytics[key] = analytics.factory(key);
-		// 		}
-		// 		analytics.load = function (key, e) {
-		// 			const t = document.createElement('script');
-		// 			t.type = 'text/javascript';
-		// 			t.async = !0;
-		// 			t.src = 'https://cdn.segment.com/analytics.js/v1/' + key + '/analytics.min.js';
-		// 			const n = document.getElementsByTagName('script')[0];
-		// 			n.parentNode.insertBefore(t, n);
-		// 			analytics._loadOptions = e;
-		// 		};
-		// 		analytics._writeKey = import.meta.env.VITE_SEGMENT_KEY;
-		// 		analytics.SNIPPET_VERSION = '4.15.3';
-		// 		analytics.load(import.meta.env.VITE_SEGMENT_KEY);
-		// 	}
+		if (!analytics.initialize)
+			if (analytics.invoked)
+				window.console && console.error && console.error('Segment snippet included twice.');
+			else {
+				analytics.invoked = !0;
+				analytics.methods = [
+					'trackSubmit',
+					'trackClick',
+					'trackLink',
+					'trackForm',
+					'pageview',
+					'identify',
+					'reset',
+					'group',
+					'track',
+					'ready',
+					'alias',
+					'debug',
+					'page',
+					'once',
+					'off',
+					'on',
+					'addSourceMiddleware',
+					'addIntegrationMiddleware',
+					'setAnonymousId',
+					'addDestinationMiddleware'
+				];
+				analytics.factory = function (e) {
+					return function () {
+						const t = Array.prototype.slice.call(arguments);
+						t.unshift(e);
+						analytics.push(t);
+						return analytics;
+					};
+				};
+				for (let e = 0; e < analytics.methods.length; e++) {
+					const key = analytics.methods[e];
+					analytics[key] = analytics.factory(key);
+				}
+				analytics.load = function (key, e) {
+					const t = document.createElement('script');
+					t.type = 'text/javascript';
+					t.async = !0;
+					t.src = 'https://cdn.segment.com/analytics.js/v1/' + key + '/analytics.min.js';
+					const n = document.getElementsByTagName('script')[0];
+					n.parentNode.insertBefore(t, n);
+					analytics._loadOptions = e;
+				};
+				analytics._writeKey = variables.segmentKey;
+				analytics.SNIPPET_VERSION = '4.15.3';
+				analytics.load(variables.segmentKey);
+			}
 
 		await userStore.subscribe((value) => {
 			if (!value && loading) {
