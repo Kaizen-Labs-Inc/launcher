b1518271ba572c7131f3a6c35f8a848e967dd61f
jordanful <j.fulghum@gmail.com>
2021-12-07 15:32:15 -0500

    get env variables working

--- svelte.config.js
+++ svelte.config.js
@@ -5,14 +5,21 @@ import vercel from '@sveltejs/adapter-vercel';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess({
-        postcss: true
-    })],
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: vercel()
+		adapter: vercel(),
+		vite: {
+			define: {
+				'process.env': process.env
+			}
+		}
 	}
 };
 
