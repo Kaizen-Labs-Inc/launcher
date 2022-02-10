b2018448e696eb9dcee746037d577ca9096b8913
Nate Vaughan <nate.vaughan@protonmail.com>
2022-02-10 16:22:50 -0600

    async logout

--- src/lib/logout.ts
+++ src/lib/logout.ts
@@ -2,8 +2,8 @@ import { signOut } from 'sk-auth/client';
 import { goto } from '$app/navigation';
 import { userStore } from '../stores/userStore';
 
-export const logout = () => {
-	signOut();
+export const logout = async () => {
+	await signOut();
 	userStore.set({ loading: false, user: undefined });
 	goto(import.meta.env.VITE_LOGOUT_URL || '/');
 };
